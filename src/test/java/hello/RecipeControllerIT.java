018d53801f43e25af97a5d93a4bd7f24d6c31865
JavaEater <hausmarke@gmx.de>
2017-03-29 22:37:11 +0200

    Solution for seperate test database

--- src/test/java/hello/RecipeControllerIT.java
+++ src/test/java/hello/RecipeControllerIT.java
@@ -14,13 +14,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations="classpath:application-test.properties")
 public class RecipeControllerIT {
 
 	@LocalServerPort
