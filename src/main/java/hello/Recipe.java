c851d84ca6c1f4747b598af86facda9778439ad7
spidersouleater <zylla.ac@gmail.com>
2017-04-05 18:31:20 +0200

    - added Recipe

---
+++ src/main/java/hello/Recipe.java
@@ -0,0 +1,61 @@
+package hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Recipe {
+
+	@Id
+	@GeneratedValue(generator = "system-uuid")
+	@GenericGenerator(name = "system-uuid", strategy = "uuid")
+	private String id;
+	private String name;
+	private String description;
+
+	protected Recipe() {
+	}
+
+	public Recipe(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("{");
+		sb.append("\"id\":\"" + id + "\",");
+		sb.append("\"name\":\"" + name + "\",");
+		sb.append("\"description\":\"" + description + "\"");
+		sb.append("}");
+		
+		return sb.toString();
+	}
+	
+}
