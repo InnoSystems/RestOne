b36e122bc3cb11f2d5351854a1094a68def6c315
JavaEater <hausmarke@gmx.de>
2017-04-12 22:25:08 +0200

    no change: important information for CORS

--- src/main/java/com/restOne/greeting/GreetingController.java
+++ src/main/java/com/restOne/greeting/GreetingController.java
@@ -2,6 +2,7 @@ package com.restOne.greeting;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,10 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    /** This service must be called from a web-server like Apache Tomcat 6 which is running on port 9090 */
+    @CrossOrigin(origins = "http://localhost:9090")
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "Kakki") String name) {
 	return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
