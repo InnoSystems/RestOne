eb2867baa64b897a9e1318a3e1620d2f563226b1
JavaEater <hausmarke@gmx.de>
2017-04-14 22:36:31 +0200

    merge RestOneClient to RestOne

--- src/main/java/com/restOne/greeting/GreetingController.java
+++ src/main/java/com/restOne/greeting/GreetingController.java
@@ -13,8 +13,6 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    /** This service must be called from a web-server like Apache Tomcat 6 which is running on port 9090 */
-    @CrossOrigin(origins = "http://localhost:9090")
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "Kakki") String name) {
 	return new Greeting(counter.incrementAndGet(), String.format(template, name));
