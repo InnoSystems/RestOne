e1ea3c490bba43d0e1f644495edf58d1d9d0d916
JavaEater <hausmarke@gmx.de>
2017-04-14 23:21:46 +0200

    eleminate Error in js

--- src/main/webapp/lib/js/hello.js
+++ src/main/webapp/lib/js/hello.js
@@ -3,19 +3,19 @@ $(function() {
 		$.ajax({
 			url : "http://localhost:8080/greeting"
 		}).then(function(data) {
-			$('.greeting1-id').append(data.id);
-			$('.greeting1-content').append(data.content);
+			$('.greeting1-id').text("The ID is: " + data.id);
+			$('.greeting1-content').text("The content is: " + data.content);
 		});
 	});
 	$("#send2").click(function() {
 		var str1 = "http://localhost:8080/greeting?name=";
-		var str2 = $("#input1").text();
+		var str2 = $("#input1").val();
 		var str3 = str1 + str2;
 		$.ajax({
 			url : str3
 		}).then(function(data) {
-			$('.greeting2-id').append(data.id);
-			$('.greeting2-content').append(data.content);
+			$('.greeting2-id').text("The ID is: " + data.id);
+			$('.greeting2-content').text("The content is: " + data.content);
 		});
 	});
 });
